39ec5c501738372d96f405d88165d8ec22b6eca2
Samsamson33 <samsamson33.0913@gmail.com>
2021-06-22 00:01:07 -0400

    Update and rename CSS reset

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom";
 import { App } from "./components/App";
 
-import "./base.css";
+import "./reset.css";
 import "tippy.js/dist/tippy.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
